f2c388f9ad8ae4979595df5ccaee22309dbb631b
Alvaritto951 <alvaritto951@gmail.com>
2022-10-11 20:11:31 +0200

    11/10/22 - botones en purchase.html (forms.py)

---
+++ cryptomonedas/models.py
@@ -0,0 +1,87 @@
+import sqlite3
+from config import *
+import requests
+from datetime import *
+def filas_to_diccionario(filas, columnas):
+
+    resultado = []
+    for fila in filas:  
+        posicion_columna = 0
+        d = {}
+        for campo in columnas:
+            d[campo[0]] = fila[posicion_columna] #Para la posición inicial del campo que sea igual a la posición de la columna
+            posicion_columna += 1
+        resultado.append(d)
+    
+    return resultado
+
+
+def select_all():
+    conn= sqlite3.connect(ORIGIN_DATA)
+    cur = conn.cursor()
+    cur.execute("SELECT id, Fecha, Hora, moneda_from, cantidad_from, Moneda_to, Cantidad_to, (cantidad_from/Cantidad_to) as PU from movements;")
+    result = filas_to_diccionario(cur.fetchall(), cur.description)
+    conn.close()
+    return result
+
+
+def insert(registro):
+  
+    conn = sqlite3.connect(ORIGIN_DATA)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO movements (Fecha, Hora, Moneda_from, Cantidad_from, Moneda_to, cantidad_to) values(?,?,?,?,?,?)", [datetime.now(), datetime.now(),registro[2],registro[3],registro[4],registro[5]])
+    conn.commit()
+    conn.close()
+
+def peticion_crypto(moneda_from_data, moneda_to_data, apikey):
+    url = requests.get(f"https://rest.coinapi.io/v1/exchangerate/{moneda_from_data}/{moneda_to_data}?&apikey={apikey}")
+    resultado = url.json()
+    return resultado
+
+def invertido():
+    conn= sqlite3.connect(ORIGIN_DATA)
+    cur = conn.cursor()
+    cur.execute("SELECT Cantidad_from FROM movements WHERE Moneda_from = 'EUR'")
+    result = filas_to_diccionario(cur.fetchall(), cur.description)
+    conn.close()
+    return result
+
+def recuperado():
+    conn= sqlite3.connect(ORIGIN_DATA)
+    cur = conn.cursor()
+    cur.execute("SELECT SUM(Cantidad_to) as Cantidad_to FROM movements WHERE Moneda_to = 'EUR'")
+    result = filas_to_diccionario(cur.fetchall(), cur.description)
+    conn.close()
+    return result
+
+def valorCompra():
+    conn= sqlite3.connect(ORIGIN_DATA)
+    cur = conn.cursor()
+    cur.execute("SELECT (SUM(Cantidad_from) - SUM(Cantidad_to ) ) as valorCompra FROM movements WHERE Moneda_to = 'EUR'")
+    result = filas_to_diccionario(cur.fetchall(), cur.description)
+    conn.close()
+    return result
+
+def valorActual():
+    conn= sqlite3.connect(ORIGIN_DATA)
+    cur = conn.cursor()
+    cur.execute("SELECT Cantidad_from FROM movements WHERE Moneda_from = 'EUR'")
+    result = filas_to_diccionario(cur.fetchall(), cur.description)
+    conn.close()
+    return result
+
+def union():
+    resultadazo = []
+    conn= sqlite3.connect(ORIGIN_DATA)
+    cur = conn.cursor()
+    cur.execute("SELECT Cantidad_from FROM movements WHERE Moneda_from = 'EUR'")
+    result = filas_to_diccionario(cur.fetchall(), cur.description)
+    resultadazo.append(result)
+    cur.execute("SELECT SUM(Cantidad_to) as Cantidad_to FROM movements WHERE Moneda_to = 'EUR'")
+    resulto = filas_to_diccionario(cur.fetchall(), cur.description)
+    resultadazo.append(resulto)
+    cur.execute("SELECT (SUM (Cantidad_from) - SUM(Cantidad_to ) ) as valorCompra FROM movements WHERE Moneda_to = 'EUR'")
+    resulta = filas_to_diccionario(cur.fetchall(), cur.description)
+    resultadazo.append(resulta)
+    conn.close()
+    return resultadazo
---
+++ cryptomonedas/routes.py
@@ -0,0 +1,71 @@
+
+from flask import redirect, render_template, request, url_for, flash
+from cryptomonedas import app
+import sqlite3
+from cryptomonedas.forms import Moneda
+from config import apikey
+from cryptomonedas.models import select_all, insert, peticion_crypto, invertido, recuperado, union, valorActual, valorCompra
+from datetime import datetime, date
+import requests
+
+
+
+@app.route("/")
+def index():
+    try:
+        registros = select_all()
+        return render_template("index.html", pageTitle = "Cryptomonedas", data =registros, cabecera = 'index.html')
+    except sqlite3.Error as e:
+        flash("Se a producido error en la base datos")
+        return render_template("index.html", pageTitle="Todos", data = [])
+
+@app.route("/purchase", methods=["GET", "POST"])
+def comprar():
+    registros = Moneda()
+    if request.method == "GET":
+        return render_template("/purchase.html", PageTitle = "Comprar", formulario = registros, cabecera = 'purchase.html')
+    
+    else:
+        try:
+            if request.values.get("submitCalcular"):
+                resultado = peticion_crypto(registros.moneda_from.data, registros.moneda_to.data, apikey)
+                tasa = resultado['rate'] * float(request.values.get("inputCantidad"))
+                total = resultado['rate'] 
+                return render_template("/purchase.html", resultado = tasa, Total = total, formulario = registros)
+        
+      
+
+            elif request.values.get("submitCompra"):
+
+                try:
+                    if registros.validate():
+                        if request.values.get('moneda_from') == request.values.get('moneda_to'):
+                            flash("Las monedas no pueden ser las mismas")
+                            return redirect(url_for('comprar'))
+
+                        resultado = peticion_crypto(registros.moneda_from.data, registros.moneda_to.data, apikey)
+                        tasa = resultado['rate'] * float(request.values.get("inputCantidad"))
+                        insert([datetime.now().time(), datetime.now().date(), resultado["asset_id_base"], request.values.get('inputCantidad'), resultado["asset_id_base"], tasa])
+                    flash("Compra realizada correctamente")
+                    return redirect(url_for('index'))
+                except sqlite3.Error as e:
+                    flash("Se ha producido error en la base datos")
+                    return redirect(url_for('index'))
+                
+                
+            else:
+                flash('Ha ocurrido un error inesperado, vuelva a intentarlo')
+                return redirect(url_for('index'))
+        
+        except:
+            flash("Has pasado limite de consultas")
+            return redirect(url_for("index"))
+
+
+@app.route("/status")
+def estado():
+    try:
+        return render_template("status.html", inv = invertido(), rec = recuperado(), vComp = valorCompra(), vAct = valorActual(), cabecera = "status.html")
+    except:
+        flash("Error de calculo intentelo mas tarde")
+        return redirect(url_for('index'))
